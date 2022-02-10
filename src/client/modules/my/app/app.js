ff6a6d54ea227f3d541fdd23729cde3001d8de36
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2022-02-10 20:40:21 +0530

    Bootstap 5

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,16 +1,5 @@
 import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
-    name = 'Salesforce Troop';
-    technologies = ['LWC OSS', 'Salesforce', 'Admin']
-    greetingmsg = 'Hello Bro';
-    changeName() {
-        this.name = 'LWC OSS';
-    }
-    get addNumber() {
-        return 1 + 2;
-    }
-    nameChange(event) {
-        this.name = event.detail;
-    }
+    
 }
