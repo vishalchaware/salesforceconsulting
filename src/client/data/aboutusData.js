7ff8e1e864e4f5d9ac096b12a659d7e78f9962c8
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2022-02-11 15:20:21 +0530

    About US

---
+++ src/client/data/aboutusData.js
@@ -0,0 +1,12 @@
+const ABOUTUS_DATA={
+    sectionHeading:'About Salesforce Troop',
+    description:"We have one of the region's best Salesforce consulting teams. We have helped a number of organizations identify, grow, deploy, and evolve their Salesforce investments. We are headquartered in the United States and have teams in Melbourne, India, and Canada.",
+    tagline:'What sets us apart is our dedication to fully comprehending your requirements prior to beginning growth.',
+    stats:[
+        {label:"Satisfied Clients",value:"50+"},
+        {label:"Certifications",value:"100+"},
+        {label:"Years Completed",value:"5+"}
+    ]
+}
+
+export default ABOUTUS_DATA
---
+++ src/client/modules/my/aboutus/aboutus.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import ABOUTUS_DATA from '../../../data/aboutusData'
+export default class Aboutus extends LightningElement {
+    aboutusData =ABOUTUS_DATA
+}
