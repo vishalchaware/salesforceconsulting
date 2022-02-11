17e3bb4b6e166d7cf2d881d227838d5a1c69dd98
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2022-02-11 21:02:40 +0530

    Client Section

---
+++ src/client/data/clientData.js
@@ -0,0 +1,38 @@
+const CLIENT_DATA={
+    sectionHeading:"What Our Clients Say's",
+    feedbacks:[
+        {
+            image:'./resources/Images/Client_section/client1.png',
+            name:'Ronne Galle',
+            role:'Project Manager',
+            feedback:"Our mission was difficult, but the Salesforce Troop team handled it admirably. It took longer than anticipated, but that was partly due to the fact that we added a number of complicated features while the project was still in progress. Salesforce Troop treated the additional requests admirably, and we are satisfied with the final product. For your Salesforce needs, I highly recommend this business."
+        },
+        {
+            name:'Missy Limana',
+            image:'./resources/Images/Client_section/client2.png',
+            role:'Engineer',
+            feedback:"Great work; the project went off without a hitch. Helped us load and integrate data, as well as configure and build fields in Salesforce, using all of our data from various excel spreadsheets. I had so much fun working with your specialists on my business project that we will come back soon!"
+        },
+        {
+            name:'Martha Brown',
+            image:'./resources/Images/Client_section/client3.png',
+            role:'Project Manager',
+            feedback:"External stakeholders benefited greatly from Salesforce Troop effective creation and integration of an enhanced and productive dashboard. The team was able to respond quickly to client feedback and maintain good contact throughout. They were meticulous and extremely professional."
+        },
+        {
+            name:'Hanna Lisem',
+            image:'./resources/Images/Client_section/client4.png',
+            role:'CTO',
+            feedback:"When it comes to evaluating project specifications, he is extremely thorough and produces high-quality work in a timely manner. I'm sure we'll work together again. Great work; the project went off without a hitch. Helped us load and integrate data, as well as configure and build fields in Salesforce, using all of our data from various excel spreadsheets."
+        }
+    ],
+    clientsLogo:[
+        './resources/Images/Companies_section/Daimler_logo.svg',
+        './resources/Images/Companies_section/Continental_logo.svg',
+        './resources/Images/Companies_section/Hulu_logo.svg',
+        './resources/Images/Companies_section/Japan_Airlines_logo.svg',
+        './resources/Images/Companies_section/Lion_Air_logo.svg',
+        './resources/Images/Companies_section/Venmo_logo.svg'
+    ]
+}
+export default CLIENT_DATA
---
+++ src/client/modules/my/client/client.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import CLIENT_DATA from '../../../data/clientData'
+export default class Clients extends LightningElement{
+    clientData = CLIENT_DATA
+}
