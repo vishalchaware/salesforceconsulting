d5915525ce603ffe652400a845f46e1a2fa76272
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2022-02-11 18:58:16 +0530

    Our Work

---
+++ src/client/data/ourworkData.js
@@ -0,0 +1,17 @@
+const OUR_WORK_DATA = {
+    sectionHeading:'Our Work',
+    points:[
+        {label:'Integration of numerous CRM solutions and modules', icon:'./resources/Images/Work_section/icon1.png'},
+        {label:'Real-time data synchronization', icon:'./resources/Images/Work_section/icon2.png'},
+        {label:'Lightning-based pixel-perfect designs using Lightning UI', icon:'./resources/Images/Work_section/icon3.png'},
+        {label:'Customization and Security in Salesforce', icon:'./resources/Images/Work_section/icon4.png'},
+        {label:'System evolution services with constant user help desk', icon:'./resources/Images/Work_section/icon5.png'},
+    ],
+    images:[
+        './resources/Images/Work_section/work1.png',
+        './resources/Images/Work_section/work2.png',
+        './resources/Images/Work_section/work3.png',
+        './resources/Images/Work_section/work4.png'
+    ]
+}
+export default OUR_WORK_DATA
---
+++ src/client/modules/my/ourwork/ourwork.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import OUR_WORK_DATA from '../../../data/ourworkData';
+
+export default class extends LightningElement {
+    ourworkData = OUR_WORK_DATA;
+}
