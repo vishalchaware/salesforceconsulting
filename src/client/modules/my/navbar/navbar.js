43fb119257867b1fc6fbbb2816ac424dd4215b83
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2022-02-12 10:26:19 +0530

    Fixed Navbar

--- src/client/modules/my/navbar/navbar.js
+++ src/client/modules/my/navbar/navbar.js
@@ -3,15 +3,26 @@ import NAV_DATA from '../../../data/navbarData.js'
 export default class Navbar extends LightningElement{
     navList = NAV_DATA
     isMobileToggle = false
-
     get openMobileNav(){
         return `collapse navbar-collapse ${this.isMobileToggle && 'show'}`
     }
 
     get navMobileDesign(){
-        return `navbar navbar-expand-lg navbar-dark ${this.isMobileToggle ? 'bg-dark' : 'purple-gradient'}  `
+        return `fixed-top navbar navbar-expand-lg navbar-dark ${this.isMobileToggle ? 'bg-dark' : 'purple-gradient'}  `
     }
     toggleMobileMenu(){
         this.isMobileToggle = !this.isMobileToggle
     }
+
+    navSelection(event){
+        event.preventDefault()
+        if(this.isMobileToggle){
+            this.toggleMobileMenu()
+        }
+        const name = event.target.name.toLowerCase().replace(/\s+/g, '')
+        let tagName = name === 'home'? 'my-navbar':`my-${name}`
+        // const elem = document.querySelector(tagName)
+        // elem.scroll();
+    }
+
 }
